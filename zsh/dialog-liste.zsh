7b2d589b0da0bde6b9b0b6128384077cedc4f7fd
chimay <orduval@gmail.com>
2018-04-22 17:14:14 +0200

    dialog liste, session

---
+++ zsh/dialog-liste.zsh
@@ -0,0 +1,27 @@
+#!/usr/bin/env /bin/zsh
+
+liste=()
+
+while read element
+do
+	liste+=$element
+done
+
+numero=1
+tagitem=()
+
+for element in $liste
+do
+	tagitem+=($numero $element)
+	(( numero += 1 ))
+done
+
+exec 3>&1
+choix=$(dialog --menu "Liste" 0 0 0 $=tagitem 2>&1 1>&3)
+exec 3>&-
+
+code=$?
+
+(( $code > 0)) && exit 1
+
+echo $~liste[$choix]
