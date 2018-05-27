80d989b4af094c2c2287ae82ae7ebeb7bbdaa78f
chimay <orduval@gmail.com>
2018-05-27 12:50:11 +0200

    lance mplayer server

--- zsh/lance-mplayer-serveur.zsh
+++ zsh/lance-mplayer-serveur.zsh
@@ -16,15 +16,37 @@ setopt warn_create_global
 
 # }}}
 
+# Déjà lancé ? {{{1
+
+alias psgrep="ps auxww | grep -v grep | grep --color=never"
+
+psgrep 'mplayer -idle -slave' &> /dev/null && {
+
+	echo "Mplayer tourne déjà"
+	echo
+
+	exit 0
+}
+
+# }}}1
+
 local tube=~/racine/run/fifo/mplayer
 
 [[ -p $tube ]] || {
 
 	[[ -e $tube ]] && rm -f $tube
 
+	mkdir -p ~/racine/run/fifo
+
 	mkfifo $tube
 }
 
 # -noconsolecontrols ?
 
-mplayer -idle -slave -input file=$tube &>> ~/log/mplayer.log &!
+mplayer \
+	-idle \
+	-slave \
+	-input file=$tube \
+	-softvol \
+	-softvol-max 150 \
+	&>>! ~/log/mplayer.log &!
