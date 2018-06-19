aec997f29ec395499dd56211f1d7463d282b8b30
chimay <orduval@gmail.com>
2018-06-19 12:04:49 +0200

    mpc-toggle

--- zsh/mpc-toggle.zsh
+++ zsh/mpc-toggle.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env /bin/zsh
 
-temps=${1:-15}
+delai_court=${1:-5}
+delai_long=${2:-60}
 
 while true
 do
@@ -10,19 +11,24 @@ do
 	pulsemixer --list-sinks | grep 'Name: VLC media player' &>/dev/null && autre=1
 	pulsemixer --list-sinks | grep 'Name: AudioIPC Server' &>/dev/null && autre=1
 
+	echo "== $(date +%H:%M:%S) =="
+	echo
 	echo Autre : $autre
 	echo
 
 	if (( autre == 0 ))
 	then
 		mpc play
+		echo
+		echo "sleep $delai_court"
+		echo
+		sleep $delai_court
 	else
 		mpc pause
+		echo
+		echo "sleep $delai_long"
+		echo
+		sleep $delai_long
 	fi
 
-	echo
-	echo "sleep $temps"
-	echo
-
-	sleep $temps
 done
