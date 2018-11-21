a3c7f8ed828dee63915e76798a8d681baa5002e6
chimay <orduval@gmail.com>
2018-11-21 15:47:46 +0100

    chrono : minutes et heures

--- zsh/chronometre.zsh
+++ zsh/chronometre.zsh
@@ -6,14 +6,38 @@ echo Use Ctrl-C to quit
 echo
 
 seconde=0
+minute=0
+heure=0
 
 while true
 do
-	sleep 1
-
 	(( seconde += 1 ))
 
 	echo -n '\t' $seconde
 
 	(( seconde % 12 == 0 )) && echo
+
+	(( seconde > 0 )) && (( seconde % 60 == 0 )) && {
+
+		seconde=0
+
+		(( minute += 1 ))
+
+		echo
+		echo $heure heu $minute min
+		echo
+	}
+
+	(( minute > 0 )) && (( minute % 60 == 0 )) && {
+
+		minute=0
+
+		(( heure += 1 ))
+
+		echo
+		echo $heure heu $minute min
+		echo
+	}
+
+	sleep 1
 done
