733bb5093026cac5e5f02fadfd4cbc205cb50f7c
chimay <orduval@gmail.com>
2020-03-12 11:18:33 +0100

    bspc event : swap if 2 windows in desktop

---
+++ zsh/bspwm-subscribe.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env /bin/zsh
+
+# vim: set filetype=zsh:
+
+bspc subscribe node_add | while read ligne
+do
+	desktop_windows=($(bspc query -N -n .window -d focused))
+	print -l $desktop_windows
+	echo
+	if [ $#desktop_windows -eq 2 ]
+	then
+		echo "Swapping"
+		echo
+		bspwm-swap.sh
+	fi
+done
