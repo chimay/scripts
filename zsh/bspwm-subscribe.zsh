1fbed8cec89a773687e7308e9ec7cdfd82bd523b
chimay <orduval@gmail.com>
2020-11-28 18:54:38 +0100

    bspwm sub

--- zsh/bspwm-subscribe.zsh
+++ zsh/bspwm-subscribe.zsh
@@ -2,8 +2,31 @@
 
 # vim: set filetype=zsh:
 
-bspc subscribe node_add | while read ligne
-do
-	bspc node -f $(echo $ligne | awk '{print $5}')
-	bspc node -s biggest.local
-done &
+presel=0
+
+bspc subscribe node_add node_presel | \
+	while read ligne
+	do
+		date +"%H:%M:%S %d %m %Y"
+		echo $ligne | grep node_presel | grep -v cancel &>/dev/null &&
+			{
+				echo Presel on
+				presel=1
+			}
+		echo $ligne | grep node_presel | grep cancel &>/dev/null &&
+			{
+				echo Presel off
+				echo Skipping next node added
+			}
+		if echo $ligne | grep node_add
+		then
+			if (( presel == 1 ))
+			then
+				presel=0
+			else
+				echo Going to big
+				bspc node -f $(echo $ligne | awk '{print $5}')
+				bspc node -s biggest.local
+			fi
+		fi
+	done &
