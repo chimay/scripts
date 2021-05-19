82e85f6b469c869c6458204d869d4d1348ffd9e6
chimay <orduval@gmail.com>
2021-05-19 11:14:32 +0200

    multi-tea

--- zsh/multi-tea.zsh
+++ zsh/multi-tea.zsh
@@ -10,6 +10,15 @@
 
 setopt extended_glob
 
+# Help
+
+[ $# -gt 0 ] && [ $1 = -h -o $1 = --help -o $1 = help ] && {
+	echo "Usage : `basename $0` [default-time [+increment-time] ]"
+	exit 0
+}
+
+# Signals
+
 TRAPINT () {
 	echo
 	echo
@@ -21,45 +30,85 @@ TRAPINT () {
 # Functions
 
 duration-prompt () {
-	if (( hour > 0 && min > 0 && sec > 0 ))
+	local hours=$1
+	local minutes=$2
+	local seconds=$3
+	if (( hours > 0 && minutes > 0 && seconds > 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$hour hour $min min $sec sec] "
-	elif (( hour > 0 && min > 0 && sec == 0 ))
+		echo -n " <$(date +%H:%M)> timer duration ? [$hours hours $minutes minutes $seconds seconds] "
+	elif (( hours > 0 && minutes > 0 && seconds == 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$hour hour $min min] "
-	elif (( hour > 0 && min == 0 && sec > 0 ))
+		echo -n " <$(date +%H:%M)> timer duration ? [$hours hours $minutes minutes] "
+	elif (( hours > 0 && minutes == 0 && seconds > 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$hour hour $sec sec] "
-	elif (( hour > 0 && min == 0 && sec == 0 ))
+		echo -n " <$(date +%H:%M)> timer duration ? [$hours hours $seconds seconds] "
+	elif (( hours > 0 && minutes == 0 && seconds == 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$hour hour] "
-	elif (( hour == 0 && min > 0 && sec > 0 ))
+		echo -n " <$(date +%H:%M)> timer duration ? [$hours hours] "
+	elif (( hours == 0 && minutes > 0 && seconds > 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$min min $sec sec] "
-	elif (( hour == 0 && min > 0 && sec == 0 ))
+		echo -n " <$(date +%H:%M)> timer duration ? [$minutes minutes $seconds seconds] "
+	elif (( hours == 0 && minutes > 0 && seconds == 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$min min] "
-	elif (( hour == 0 && min == 0 && sec > 0 ))
+		echo -n " <$(date +%H:%M)> timer duration ? [$minutes minutes] "
+	elif (( hours == 0 && minutes == 0 && seconds > 0 ))
 	then
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$sec sec] "
+		echo -n " <$(date +%H:%M)> timer duration ? [$seconds seconds] "
 	else
 		echo
-		echo -n " <$(date +%H:%M)> timer duration ? [$min min] "
+		echo -n " <$(date +%H:%M)> timer duration ? [$minutes minutes] "
+	fi
+}
+
+minuter-info () {
+	local hours=$1
+	local minutes=$2
+	local seconds=$3
+	if (( hours > 0 && minutes > 0 && seconds > 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $hours hours $minutes minutes $seconds seconds"
+	elif (( hours > 0 && minutes > 0 && seconds == 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $hours hours $minutes minutes"
+	elif (( hours > 0 && minutes == 0 && seconds > 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $hours hours $seconds seconds"
+	elif (( hours > 0 && minutes == 0 && seconds == 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $hours hours"
+	elif (( hours == 0 && minutes > 0 && seconds > 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $minutes minutes $seconds seconds"
+	elif (( hours == 0 && minutes > 0 && seconds == 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $minutes minutes"
+	elif (( hours == 0 && minutes == 0 && seconds > 0 ))
+	then
+		echo
+		echo "	[$(date +%H:%M)] Ding dong in $seconds seconds"
+	else
+		echo
+		echo "	[$(date +%H:%M)] Ding dong now"
 	fi
 }
 
 # Initialisation
 
-duration=0:1:0
-
-augment=0
+tempus=0:1:0
+increment=0:0:0
 
 # Arguments
 
@@ -73,7 +122,7 @@ do
 		[0-9./:+]##)
 			tableau=(${(s/+/)1})
 			tempus=$tableau[1]
-			augment=$tableau[2]
+			increment=$tableau[2]
 			shift
 			;;
 		*)
@@ -83,71 +132,51 @@ do
 done
 
 duration=$(canonical-duration.zsh $tempus)
-tableau=(${(s/:/)duration})
-hour=$tableau[1]
-min=$tableau[2]
-sec=$tableau[3]
+duratab=(${(s/:/)duration})
+hours=$duratab[1]
+minutes=$duratab[2]
+seconds=$duratab[3]
 
-if [ $augment -gt 0 ]
+augment=$(canonical-duration.zsh $increment)
+augmentab=(${(s/:/)augment})
+added_hours=$augmentab[1]
+added_minutes=$augmentab[2]
+added_seconds=$augmentab[3]
+
+if [ $augment != 0:0:0 ]
 then
-	echo Default duration will be increased by $augment seconds each time.
+	echo Default duration will be increased by $augment each time.
 fi
 
-duration-prompt
+duration-prompt $hours $minutes $seconds
 
 while read tempus
 do
 	if [ $#tempus -eq 0 ]
 	then
-		tempus=$hour:$min:$sec
+		tempus=$hours:$minutes:$seconds
 	elif [ $tempus = q -o $tempus = quit ]
 	then
 		# q to quit
 		break
 	fi
 	duration=$(canonical-duration.zsh $tempus)
-	tableau=(${(s/:/)duration})
-	hour=$tableau[1]
-	min=$tableau[2]
-	sec=$tableau[3]
-	if (( hour > 0 && min > 0 && sec > 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $hour hour $min min $sec sec"
-	elif (( hour > 0 && min > 0 && sec == 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $hour hour $min min"
-	elif (( hour > 0 && min == 0 && sec > 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $hour hour $sec sec"
-	elif (( hour > 0 && min == 0 && sec == 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $hour hour"
-	elif (( hour == 0 && min > 0 && sec > 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $min min $sec sec"
-	elif (( hour == 0 && min > 0 && sec == 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $min min"
-	elif (( hour == 0 && min == 0 && sec > 0 ))
-	then
-		echo
-		echo "	[$(date +%H:%M)] Ding dong in $sec sec"
-	else
-		echo
-		echo "	[$(date +%H:%M)] Ding dong now"
-	fi
-	minuter-ding.zsh $hour:$min:$sec &> /dev/null
-	(( sec += augment ))
-	duration=$(canonical-duration.zsh $hour:$min:$sec)
-	tableau=(${(s/:/)duration})
-	hour=$tableau[1]
-	min=$tableau[2]
-	sec=$tableau[3]
-	duration-prompt
+	duratab=(${(s/:/)duration})
+	hours=$duratab[1]
+	minutes=$duratab[2]
+	seconds=$duratab[3]
+	# launch minuter
+	minuter-info $hours $minutes $seconds
+	ding-dong.zsh $hours:$minutes:$seconds &> /dev/null
+	# add increment for next minuter
+	(( hours += added_hours ))
+	(( minutes += added_minutes ))
+	(( seconds += added_seconds ))
+	duration=$(canonical-duration.zsh $hours:$minutes:$seconds)
+	duratab=(${(s/:/)duration})
+	hours=$duratab[1]
+	minutes=$duratab[2]
+	seconds=$duratab[3]
+	# prompt
+	duration-prompt $hours $minutes $seconds
 done
