e4903a3a77f65fa24df5d406b527ac13bb77a64e
chimay <remi.dore@protonmail.com>
2023-10-22 06:49:23 +0200

    run greenclip : backup of conf file

---
+++ shell/run/run-greenclip.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env sh
+
+backup=$hOME/racine/config/windenv/greenclip/greenclip.cfg.bak
+config=$hOME/racine/config/windenv/greenclip/greenclip.cfg
+
+cp -f "$backup" "$config"
+
+greenclip daemon &
