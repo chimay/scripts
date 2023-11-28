e72c1e11130626f013307ad869f59881cdb6cf7d
chimay <remi.dore@protonmail.com>
2023-11-28 16:48:25 +0100

    improve run-timidity-server

--- shell/multimedia/run-timidity-server.sh
+++ shell/multimedia/run-timidity-server.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
-pgrep -af timidity > /dev/null && {
+if pgrep -af timidity | grep -v "$0" > /dev/null
+then
 	echo Timidity is already running.
 	exit 0
-}
+fi
 
 echo timidity -B1,8 -iA
 timidity -B1,8 -iA &
