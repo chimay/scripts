25d804579f3b2786ed90d89be3f129c3c6935c8d
chimay <remi.dore@protonmail.com>
2024-06-24 08:15:49 +0200

    minor

--- shell/windenv/urxvtc-calculator.zsh
+++ shell/windenv/urxvtc-calculator.zsh
@@ -3,16 +3,16 @@
 # Credit:
 # https://www.reddit.com/r/bspwm/comments/85hr4c/making_a_scratchpaddropdown_terminal_in_bspwm/
 
-id=$(xdo id -N URxvt -n calculator)
+iden=$(xdo id -N URxvt -n calculator)
 
-echo id = $id
+echo iden = $iden
 echo
 
-if [ -z "$id" ]; then
+if [ -z "$iden" ]; then
 	urxvtc -name calculator -e calc
 else
 	action='hide';
-	if [[ $(xprop -id $id | awk '/window state: / {print $3}') == 'Withdrawn' ]]; then
+	if [[ $(xprop -id $iden | awk '/window state: / {print $3}') == 'Withdrawn' ]]; then
 		action='show';
 	fi
 	xdo $action -N URxvt -n calculator
