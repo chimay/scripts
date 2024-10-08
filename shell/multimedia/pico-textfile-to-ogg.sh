c8526aaed77071213f0557fdb181d25945a643e6
chimay <remi.dore@protonmail.com>
2024-10-08 17:26:43 +0200

    pico tts : textfile & words to ogg

---
+++ shell/multimedia/pico-textfile-to-ogg.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+textfile=${1:-"textfile"}
+language=${2:-"en-US"}
+
+rootname=${textfile%.*}
+
+echo "pico-tts -l fr-FR < $textfile > $rootname.wav"
+echo
+pico-tts -l fr-FR < $textfile > $rootname.raw
+
+# listen to the result :
+# aplay -q -f S16_LE -r 16 $rootname.wav
+
+echo
+echo "------------------------------"
+echo
+
+echo sox -r 16k -e signed -b 16 $rootname.raw -c 1 -b 16 $rootname.wav
+echo
+sox -r 16k -e signed -b 16 $rootname.raw -c 1 -b 16 $rootname.wav
+
+echo
+echo "------------------------------"
+echo
+
+echo ffmpeg-audio.zsh ogg $rootname.wav
+echo
+ffmpeg-audio.zsh ogg $rootname.wav
+
+echo
+echo "------------------------------"
+echo
+
+echo mv $rootname.ogg $rootname-tmp.ogg
+echo
+mv $rootname.ogg $rootname-tmp.ogg
+
+echo
+echo "------------------------------"
+echo
+
+echo sox $rootname-tmp.ogg $rootname.ogg pad 0.5 0.5
+echo
+sox $rootname-tmp.ogg $rootname.ogg pad 0.5 0.5
+
+echo
+echo "------------------------------"
+echo
+
+echo rm -rf $rootname.raw $rootname.wav $rootname-tmp.ogg
+echo
+#rm -rf $rootname.raw $rootname.wav $rootname-tmp.ogg
---
+++ shell/multimedia/pico-words-to-ogg.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+audiofile=${1:-"audiofile"}
+text=${2:-"Example text."}
+language=${3:-"en-US"}
+
+echo pico2wave -l fr-FR -w $audiofile.wav $text
+echo
+pico2wave -l fr-FR -w $audiofile.wav "$text"
+
+echo
+echo "------------------------------"
+echo
+
+echo ffmpeg-audio.zsh ogg $audiofile.wav
+echo
+ffmpeg-audio.zsh ogg $audiofile.wav
+
+echo
+echo "------------------------------"
+echo
+
+echo mv $audiofile.ogg $audiofile-tmp.ogg
+echo
+mv $audiofile.ogg $audiofile-tmp.ogg
+
+echo
+echo "------------------------------"
+echo
+
+echo sox $audiofile-tmp.ogg $audiofile.ogg pad 0.5 0.5
+echo
+sox $audiofile-tmp.ogg $audiofile.ogg pad 0.5 0.5
+
+echo
+echo "------------------------------"
+echo
+
+echo rm -rf $audiofile.wav $audiofile-tmp.ogg
+echo
+rm -rf $audiofile.wav $audiofile-tmp.ogg
