689b62349ac3e2e7486e3c37b4182a4ac9260dce
chimay <remi.dore@protonmail.com>
2025-08-07 09:42:50 +0200

    askpass rofi

--- shell/windenv/askpass-rofi.sh
+++ shell/windenv/askpass-rofi.sh
@@ -3,4 +3,6 @@
 # Credit: https://www.reddit.com/r/voidlinux/comments/kb4how/how_to_execute_suspend_and_hibernate_from_the/
 #         https://www.reddit.com/r/qtools/comments/dwc02a/rofiquestion_how_to_hide_the_textbox/
 
-exec rofi -dmenu -p 'Password ' -theme-str 'entry { enabled: false;}' 2> ~/log/rofi.err
+# See also ~/racine/config/windenv/rofi/themes/invisible.rasi
+
+exec rofi -dmenu -p 'Password ' -theme invisible 2> ~/log/rofi.err
